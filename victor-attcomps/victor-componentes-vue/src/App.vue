7833c230877acf3c3c1ee259a85421f89a82ee16
Victor Luiz da Silva <99228756+VictorLDS@users.noreply.github.com>
2022-04-27 23:34:05 -0300

    Add files via upload

---
+++ victor-attcomps/victor-componentes-vue/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import MenuLateral from "@/components/MenuLateral.vue";
+import BlocoCabecalho from "@/components/BlocoCabecalho.vue"
+import BlocoRodape from "@/components/BlocoRodape.vue"
+import BlocoPrincipal from "@/components/BlocoPrincipal.vue"
+export default {
+  components: { MenuLateral, BlocoCabecalho, BlocoRodape, BlocoPrincipal },
+};
+</script>
+<template>
+  <MenuLateral />
+  <BlocoCabecalho />
+  <BlocoPrincipal />
+  <BlocoRodape />
+</template>
+<style></style>
---
+++ victor-attcomps/victor-componentes-vue/src/components/BlocoPrincipal.vue
@@ -0,0 +1,16 @@
+<script>
+import BlocoNovo from "@/components/BlocoNovo.vue"
+export default {
+    components: {BlocoNovo},
+};
+</script>
+<template>
+    <main id="content">
+        <BlocoNovo class="BlocoNovo" />
+        <BlocoNovo class="BlocoNovo"/>
+        <BlocoNovo class="BlocoNovo"/>
+        <BlocoNovo class="BlocoNovo"/>
+        <BlocoNovo class="BlocoNovo"/>
+        <BlocoNovo class="BlocoNovo"/>
+    </main>
+</template>
